644b2783a6aa8a67d9a2777ecc1686773993dc29
Zelaux <ura20060605@gmail.com>
2021-01-02 20:09:09 +0300

    Add BlockSwitcher.java

---
+++ src/braindustry/world/blocks/sandbox/BlockSwitcher.java
@@ -0,0 +1,195 @@
+package braindustry.ModContent.world.blocks.sandbox;
+
+import arc.Core;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Lines;
+import arc.graphics.g2d.TextureRegion;
+import arc.math.Angles;
+import arc.math.Mathf;
+import arc.struct.Seq;
+import arc.util.Time;
+import arc.util.io.Reads;
+import arc.util.io.Writes;
+import mindustry.Vars;
+import mindustry.game.Team;
+import mindustry.gen.Building;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Pal;
+import mindustry.io.JsonIO;
+import mindustry.logic.LAccess;
+import mindustry.world.Block;
+import mindustry.world.blocks.ControlBlock;
+import mindustry.world.blocks.power.PowerGraph;
+
+public class BlockSwitcher extends Block {
+    public TextureRegion laser;
+    public TextureRegion laserEnd;
+    public float laserRange = 6.0F;
+    public Color laserColor1;
+    public Color laserColor2;
+
+    public BlockSwitcher(String name) {
+        super(name);
+        this.update = true;
+        this.solid = true;
+        this.laserColor1 = Color.white;
+        this.laserColor2 = Pal.powerLight;
+        this.configurable = true;
+        this.swapDiagonalPlacement = true;
+        this.config(Integer.class, (e, value) -> {
+            BlockSwitcherBuild entity = (BlockSwitcherBuild) e;
+            Building other = Vars.world.build(value);
+            boolean contains = entity.links.contains(other);
+            boolean valid = other != null && other.power != null;
+            boolean good = other.dst(entity) <= BlockSwitcher.this.laserRange;
+            entity.handleBuilding(other);
+
+        });
+    }
+
+    @Override
+    public void load() {
+        super.load();
+        this.laser = Core.atlas.find("laser");
+        this.laserEnd = Core.atlas.find("laser-end");
+    }
+
+    protected void drawLaser(Team team, float x1, float y1, float x2, float y2, int size1, int size2, boolean enable) {
+        float angle1 = Angles.angle(x1, y1, x2, y2);
+        float vx = Mathf.cosDeg(angle1);
+        float vy = Mathf.sinDeg(angle1);
+        float len1 = (float) (size1 * 8) / 2.0F - 1.5F;
+        float len2 = (float) (size2 * 8) / 2.0F - 1.5F;
+        Draw.color(enable ? Color.green : Pal.redDust);
+        Drawf.laser(team, this.laser, this.laserEnd, x1 + vx * len1, y1 + vy * len1, x2 - vx * len2, y2 - vy * len2, 0.25F);
+    }
+
+    public boolean goodBuilding(BlockSwitcherBuild forB, Building other) {
+        return other.dst(forB) <= (laserRange + other.block.size+Mathf.ceil(forB.block.size/2f)) * 8f && forB != other && forB.isValid() && other.isValid();
+    }
+
+    public class BlockSwitcherBuild extends Building {
+        Seq<Building> links = new Seq<>();
+
+        public void placed() {
+            links.clear();
+        }
+
+
+        public void dropped() {
+            this.power.links.clear();
+            this.power.graph = new PowerGraph();
+            this.power.graph.add(this);
+        }
+        private Seq<Runnable> onUpdate=new Seq<>();
+        public void updateTile() {
+            onUpdate.each(Runnable::run);
+            onUpdate.clear();
+            Seq<Building> catchLinks = links.copy();
+            catchLinks.each((link) -> {
+                if (!BlockSwitcher.this.goodBuilding(this, link)) links.remove(link);
+            });
+            links.each(link -> {
+                boolean enable = (link instanceof ControlBlock && ((ControlBlock) link).isControlled() && !(link instanceof BlockSwitcherBuild));
+                link.control(LAccess.enabled, enable ? 1 : 0, 0, 0, 0);
+                link.enabledControlTime = 30.0F;
+            });
+        }
+
+        public boolean handleBuilding(Building other) {
+
+            if (!this.links.contains(other) && BlockSwitcher.this.goodBuilding(this, other)) {
+                this.links.add(other);
+                return true;
+            } else if (this.links.contains(other) && BlockSwitcher.this.goodBuilding(this, other)) {
+                this.links.remove(other);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public boolean onConfigureTileTapped(Building other) {
+            if (handleBuilding(other)) {
+//                this.deselect();
+                return false;
+            }
+            return true;
+        }
+
+        public void drawSelect() {
+            super.drawSelect();
+            Lines.stroke(1.0F);
+            Draw.color(Pal.accent);
+            Drawf.circles(this.x, this.y, (BlockSwitcher.this.laserRange+Mathf.ceil(block.size/2f)) * 8.0F);
+            Draw.reset();
+        }
+
+        public void drawConfigure() {
+            Drawf.circles(this.x, this.y, (float) (this.tile.block().size * 8) / 2.0F + 1.0F + Mathf.absin(Time.time, 4.0F, 1.0F));
+            Drawf.circles(this.x, this.y, BlockSwitcher.this.laserRange * 8.0F);
+
+            for (int x = (int) ((float) this.tile.x - BlockSwitcher.this.laserRange - 2.0F); (float) x <= (float) this.tile.x + BlockSwitcher.this.laserRange + 2.0F; ++x) {
+                for (int y = (int) ((float) this.tile.y - BlockSwitcher.this.laserRange - 2.0F); (float) y <= (float) this.tile.y + BlockSwitcher.this.laserRange + 2.0F; ++y) {
+                    Building link = Vars.world.build(x, y);
+                    boolean linked = this.linked(link);
+                    if (linked) {
+                        Drawf.square(link.x, link.y, (float) (link.block.size * 8) / 2.0F + 1.0F, Pal.place);
+                    }
+
+                }
+            }
+
+            Draw.reset();
+        }
+
+        public void draw() {
+            super.draw();
+            if (!Mathf.zero(Vars.renderer.laserOpacity)) {
+                Draw.z(70.0F);
+                this.links.each((link) -> {
+                    BlockSwitcher.this.drawLaser(this.team, this.x, this.y, link.x, link.y, BlockSwitcher.this.size, link.block.size, link.enabled);
+                });
+
+                Draw.reset();
+            }
+        }
+
+
+        public String config() {
+            Seq<Integer> posses = new Seq<>();
+            links.each((building -> {
+                posses.add(building.pos());
+            }));
+            return JsonIO.json().toJson(posses);
+        }
+
+        protected void readConfig(String config) {
+            onUpdate.add(()->{
+                Seq<Integer> posses = JsonIO.json().fromJson(Seq.class, config);
+                links.clear();
+                posses.each((pos) -> {
+                    Building build = Vars.world.build(pos);
+                    if (build != null && !linked(build)) links.add(build);
+                });
+            });
+        }
+
+        @Override
+        public void write(Writes write) {
+            super.write(write);
+            write.str(config());
+        }
+
+        @Override
+        public void read(Reads read) {
+            super.read(read);
+            readConfig(read.str());
+        }
+
+        protected boolean linked(Building other) {
+            return this.links.contains(other);
+        }
+    }
+}
