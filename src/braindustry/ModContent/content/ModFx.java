2f571900b48b50003fb64439fb69a3a271fa259e
Mya4ta <sefce3543@gmail.com>
2021-01-02 15:55:54 +0300

    refactor ModFx codestyle

--- src/braindustry/ModContent/content/ModFx.java
+++ src/braindustry/ModContent/content/ModFx.java
@@ -13,25 +13,24 @@ import mindustry.graphics.Drawf;
 import static braindustry.modVars.modFunc.print;
 
 public class ModFx {
-
-
-
-
     static {
         Fx.class.isArray();
     }
+
     public static final Effect shootSmall = new Effect(8.0F, (e) -> {
         Draw.color(Color.white, e.color, e.fin());
         float w = 1.0F + 5.0F * e.fout();
         Drawf.tri(e.x, e.y, w, 15.0F * e.fout(), e.rotation);
         Drawf.tri(e.x, e.y, w, 3.0F * e.fout(), e.rotation + 180.0F);
     });
-    public static Effect purpleLaserChargeSmall =new Effect(40.0F, 100.0F, (e) -> {
+
+    public static Effect purpleLaserChargeSmall = new Effect(40.0F, 100.0F, (e) -> {
         Draw.color(Color.valueOf("d5b2ed"));
         Lines.stroke(e.fin() * 2.0F);
         Lines.circle(e.x, e.y, e.fout() * 50.0F);
     });
-    public static Effect purpleBomb =new Effect(40.0F, 100.0F, (e) -> {
+
+    public static Effect purpleBomb = new Effect(40.0F, 100.0F, (e) -> {
         Color color=Color.valueOf("d5b2ed");
         Draw.color(color);
         Lines.stroke(e.fout() * 2.0F);
@@ -50,8 +49,9 @@ public class ModFx {
         }
 
     });
-    public static Effect lacertaLaserCharge =new Effect(80.0F, 100.0F, (e) -> {
-        print("e.data: @",e.data);
+
+    public static Effect lacertaLaserCharge = new Effect(80.0F, 100.0F, (e) -> {
+        print("e.data: @", e.data);
         Color color=Color.valueOf("d5b2ed");
         Draw.color(color);
         Lines.stroke(e.fin() * 2.0F);
@@ -63,7 +63,8 @@ public class ModFx {
         Draw.color();
         Fill.circle(e.x, e.y, e.fin() * 10.0F);
     });
-    public static Effect yellowBomb =new Effect(40.0F, 100.0F, (e) -> {
+
+    public static Effect yellowBomb = new Effect(40.0F, 100.0F, (e) -> {
         Color color=Color.valueOf("FFFD99");
         Draw.color(color);
         Lines.stroke(e.fout() * 2.0F);
@@ -80,13 +81,14 @@ public class ModFx {
         for(i = 0; i < 4; ++i) {
             Drawf.tri(e.x, e.y, 3.0F, 35.0F * e.fout(), (float)(i * 90));
         }
+    });
 
-    });;
     public static final Effect absorb = new Effect(12.0F, (e) -> {
         Draw.color(e.color);
         Lines.stroke(2.0F * e.fout());
         Lines.circle(e.x, e.y, 5.0F * e.fout());
     });
+
     public static final Effect changeCraft = new Effect(40.0F, (e) -> {
         Color secondColor;
         if (e.data == null) {
@@ -99,6 +101,7 @@ public class ModFx {
         Draw.alpha(e.fout());
         Fill.square(e.x, e.y, 4.0F * e.rotation);
     });
+
     public static final Effect debugSelect = new DebugEffect(40.0F, (e) -> {
         Color secondColor;
         if (e.data == null) {
