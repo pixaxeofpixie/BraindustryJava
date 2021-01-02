2c7e5835faf8966e109b88deba593e0b75f645bb
Mya4ta <sefce3543@gmail.com>
2021-01-02 16:11:17 +0300

    ModSounds code cleanup

--- src/braindustry/ModContent/content/ModSounds.java
+++ src/braindustry/ModContent/content/ModSounds.java
@@ -5,13 +5,15 @@ import arc.audio.Sound;
 import mindustry.ctype.ContentList;
 
 public class ModSounds implements ContentList {
-    public static Sound electronShoot=new Sound();
-    public static Sound electronCharge=new Sound();
+    public static Sound electronShoot = new Sound();
+    public static Sound electronCharge = new Sound();
+
     @Override
     public void load() {
         Core.assets.load("sounds/electronShoot.ogg", Sound.class).loaded = (a) -> {
             electronShoot = (Sound)a;
         };
+
         Core.assets.load("sounds/electronCharge.ogg", Sound.class).loaded = (a) -> {
             electronCharge = (Sound)a;
         };
