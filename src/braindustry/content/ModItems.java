4c4003cf47792d7a30792b30da49824d04a00cc8
Antarex <63517945+pixaxeofpixie@users.noreply.github.com>
2021-01-07 23:34:38 +0300

    astronomical-alloy

--- src/braindustry/content/ModItems.java
+++ src/braindustry/content/ModItems.java
@@ -10,7 +10,7 @@ public class ModItems implements ContentList {
         Items.class.isArray();
     }
 
-    public static Item chromium, exoticAlloy, graphenite, odinum, phaseAlloy, plastic;
+    public static Item chromium, exoticAlloy, graphenite, odinum, phaseAlloy, plastic, astroAlloy;
 
     public void load() {
         chromium = new Item("chromium") {
@@ -81,6 +81,19 @@ public class ModItems implements ContentList {
                 this.radioactivity=0.01f;
                 this.cost=4;
                 this.color = Color.valueOf("f6bfff");
+            }
+        };
+        astroAlloy  = new Item("astronomical-alloy") {
+            {
+                this.localizedName="Astronomical Bar";
+                this.description="Super-heavy and durable syntethic metal, can be only obtained from high-level bosses.";
+                this.cost = 3;
+                this.alwaysUnlocked = true;
+                this.explosiveness = 0.98f;
+                this.color = Color.valueOf("e868d7");
+                this.radioactivity = 0.94f;
+                this.flammability = 0.99f;
+                this.hardness= 999;
             }
         };
     }
