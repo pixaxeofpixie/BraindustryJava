ceef57c9608fedd99e2f24f583f58a3062ffcb2f
Zelaux <ura20060605@gmail.com>
2021-01-25 21:58:44 +0300

    Separated the power section

--- src/braindustry/content/ModBlocks.java
+++ src/braindustry/content/ModBlocks.java
@@ -2,26 +2,22 @@ package braindustry.content;
 
 import arc.Core;
 import arc.graphics.Color;
-import arc.graphics.g2d.Draw;
 import arc.math.Mathf;
 import arc.struct.Seq;
 import arc.util.Time;
-import braindustry.content.Blocks.ModUnitsBlocks;
 import braindustry.content.Blocks.ModDefense;
 import braindustry.content.Blocks.ModOtherBlocks;
 import braindustry.content.Blocks.ModProduction;
-import braindustry.type.Recipe;
+import braindustry.content.Blocks.ModUnitsBlocks;
 import braindustry.world.blocks.TestBlock;
 import braindustry.world.blocks.Unit.power.UnitPowerGenerator;
 import braindustry.world.blocks.Unit.power.UnitPowerNode;
 import braindustry.world.blocks.distribution.SmartRouter;
-import braindustry.world.blocks.production.MultiCrafter;
 import braindustry.world.blocks.sandbox.BlockSwitcher;
 import braindustry.world.blocks.sandbox.DpsMeter;
 import braindustry.world.blocks.sandbox.UnitSpawner;
 import mindustry.content.Blocks;
 import mindustry.content.Items;
-import mindustry.content.Liquids;
 import mindustry.ctype.ContentList;
 import mindustry.graphics.Pal;
 import mindustry.logic.LAccess;
@@ -34,7 +30,6 @@ import mindustry.world.blocks.distribution.BufferedItemBridge;
 import mindustry.world.blocks.distribution.ItemBridge;
 import mindustry.world.blocks.storage.StorageBlock;
 import mindustry.world.meta.BuildVisibility;
-import mindustryAddition.type.ModLiquidStack;
 import mindustryAddition.world.blocks.distribution.CrossBufferedItemBridge;
 import mindustryAddition.world.blocks.distribution.CrossItemBridge;
 
@@ -44,9 +39,12 @@ public class ModBlocks implements ContentList {
 //transportation
             armoredPlastaniumConveyor, chromiumConduit, phaseAlloyConveyor, plasticConveyor, surgeConveyor, surgePayloadConveyor,
 
-    //environment and power
-    magmaFloor, obsidianBlock, obsidianFloor, oreChromium, oreOdinum, differentialMagmaGenerator,
-            grapheniteSolarCollectorLarge, magmaGenerator, odinumReactor, blackHoleReactor, phaseAlloySolarPanel, phaseTower, crimzesWall, crimzesFloor, jungleWall, jungleFloor, jungleWater, dirtRocksWall,
+    //environment
+    magmaFloor, obsidianBlock, obsidianFloor, oreChromium, oreOdinum,
+            crimzesWall, crimzesFloor, jungleWall, jungleFloor, jungleWater, dirtRocksWall,
+
+    //power
+    differentialMagmaGenerator, grapheniteSolarCollectorLarge, magmaGenerator, odinumReactor, blackHoleReactor, phaseAlloySolarPanel, phaseTower,
 
     //production
     refrigerantReactor, chromiumForge, exoticAlloySmelter, grapheniteFluidizer, grapheniteForge,
@@ -65,8 +63,8 @@ public class ModBlocks implements ContentList {
             plasticWall, astronomicalWall, largeAstronomicalWall,
 
     //experimental
-    smartRouter, turretSwitcher,blockHealer, dpsMeter, unitGenerator,unitNode, multiCrafter, largeMultiCrafter, unitSpawner,
-            exampleCrossItemBridge, exampleCrossPhaseBridge,testBlock;
+    smartRouter, turretSwitcher, blockHealer, dpsMeter, unitGenerator, unitNode, multiCrafter, largeMultiCrafter, unitSpawner,
+            exampleCrossItemBridge, exampleCrossPhaseBridge, testBlock;
 
 
     public void load() {
@@ -74,13 +72,13 @@ public class ModBlocks implements ContentList {
         new ModProduction().load();
         new ModOtherBlocks().load();
         new ModDefense().load();
-        testBlock=new TestBlock("test-block"){
+        testBlock = new TestBlock("test-block") {
             {
                 this.size = 2;
                 this.requirements(Category.logic, ItemStack.with(), true);
             }
         };
-        exampleCrossPhaseBridge =new CrossItemBridge("cross-phase-conveyor"){
+        exampleCrossPhaseBridge = new CrossItemBridge("cross-phase-conveyor") {
             {
                 this.requirements(Category.distribution, ItemStack.with(Items.phaseFabric, 5, Items.silicon, 7, Items.lead, 10, Items.graphite, 10));
                 this.range = 12;
@@ -88,57 +86,58 @@ public class ModBlocks implements ContentList {
                 this.hasPower = true;
                 this.consumes.power(0.3F);
                 /** custom connect filter*/
-                connectFilter=(build)->{
-                    Block block=build.block;
+                connectFilter = (build) -> {
+                    Block block = build.block;
                     return block.acceptsItems || block instanceof StorageBlock;
                 };
                 //end of block
-                ItemBridge bridge=(ItemBridge)Blocks.phaseConveyor;
-                range=bridge.range;
+                ItemBridge bridge = (ItemBridge) Blocks.phaseConveyor;
+                range = bridge.range;
             }
+
             @Override
             public void load() {
-                ItemBridge bridge=(ItemBridge)Blocks.phaseConveyor;
-                Core.atlas.addRegion(this.name,Core.atlas.find(bridge.name));
+                ItemBridge bridge = (ItemBridge) Blocks.phaseConveyor;
+                Core.atlas.addRegion(this.name, Core.atlas.find(bridge.name));
                 super.load();
-                this.arrowRegion=bridge.arrowRegion;
-                this.bridgeRegion=bridge.bridgeRegion;
-                this.endRegion=bridge.endRegion;
-                this.region=bridge.region;
+                this.arrowRegion = bridge.arrowRegion;
+                this.bridgeRegion = bridge.bridgeRegion;
+                this.endRegion = bridge.endRegion;
+                this.region = bridge.region;
             }
         };
-        exampleCrossItemBridge =new CrossBufferedItemBridge("cross-item-bridge"){
+        exampleCrossItemBridge = new CrossBufferedItemBridge("cross-item-bridge") {
             {
                 this.requirements(Category.distribution, ItemStack.with(Items.lead, 6, Items.copper, 6));
                 this.range = 4;
                 this.speed = 74.0F;
                 this.bufferCapacity = 14;
                 /** custom connect filter*/
-                connectFilter=(build)->{
-                    Block block=build.block;
+                connectFilter = (build) -> {
+                    Block block = build.block;
                     return block.acceptsItems || block instanceof StorageBlock;
                 };
                 /** default filter check blocks from connectBlocksGetter*/
-                connectBlocksGetter=()->{
+                connectBlocksGetter = () -> {
                     return Seq.with(Blocks.titaniumConveyor);
                 };
                 //end of block
-                BufferedItemBridge bridge=(BufferedItemBridge)Blocks.itemBridge;
-                range=bridge.range;
+                BufferedItemBridge bridge = (BufferedItemBridge) Blocks.itemBridge;
+                range = bridge.range;
             }
 
             @Override
             public void load() {
-                BufferedItemBridge bridge=(BufferedItemBridge)Blocks.itemBridge;
-                Core.atlas.addRegion(this.name,Core.atlas.find(bridge.name));
+                BufferedItemBridge bridge = (BufferedItemBridge) Blocks.itemBridge;
+                Core.atlas.addRegion(this.name, Core.atlas.find(bridge.name));
                 super.load();
-                this.arrowRegion=bridge.arrowRegion;
-                this.bridgeRegion=bridge.bridgeRegion;
-                this.endRegion=bridge.endRegion;
-                this.region=bridge.region;
+                this.arrowRegion = bridge.arrowRegion;
+                this.bridgeRegion = bridge.bridgeRegion;
+                this.endRegion = bridge.endRegion;
+                this.region = bridge.region;
             }
         };
-        unitSpawner=new UnitSpawner("unit-spawner"){
+        unitSpawner = new UnitSpawner("unit-spawner") {
             {
                 this.size = 2;
                 this.requirements(Category.effect, ItemStack.empty);
@@ -166,42 +165,42 @@ public class ModBlocks implements ContentList {
         turretSwitcher = new BlockSwitcher("turret-switcher") {
             {
                 /** custom block filter*/
-                blockFilter=(build)->{
+                blockFilter = (build) -> {
                     return build.block instanceof Turret;
                 };
-                colorFunc=(b)-> Color.orange.cpy().lerp(Pal.accent,0.1f);
+                colorFunc = (b) -> Color.orange.cpy().lerp(Pal.accent, 0.1f);
                 /** default action*/
-                action =(b)-> {
-                    Turret.TurretBuild build =(Turret.TurretBuild)b;
-                    boolean enable= (build instanceof ControlBlock && ((ControlBlock) build).isControlled());
+                action = (b) -> {
+                    Turret.TurretBuild build = (Turret.TurretBuild) b;
+                    boolean enable = (build instanceof ControlBlock && ((ControlBlock) build).isControlled());
                     build.control(LAccess.enabled, enable ? 1 : 0, 0, 0, 0);
                     build.enabledControlTime = 30.0F;
-                    if (enable){
-                        build.enabledControlTime=0f;
-                        build.charging=false;
+                    if (enable) {
+                        build.enabledControlTime = 0f;
+                        build.charging = false;
                     }
                 };
                 this.size = 2;
                 this.laserRange = 6.0F;
-                this.health=10000;
+                this.health = 10000;
                 this.requirements(Category.distribution, BuildVisibility.sandboxOnly, ItemStack.with(Items.copper, 3, Items.silicon, 10));
                 this.buildCostMultiplier = 4.0F;
             }
         };
         blockHealer = new BlockSwitcher("block-healer") {
             {
-                blockFilter=(build)->{
+                blockFilter = (build) -> {
                     return true;
                 };
-                action =(build)-> {
-                    build.heal(build.delta()*build.maxHealth);
+                action = (build) -> {
+                    build.heal(build.delta() * build.maxHealth);
                 };
-                colorFunc=(b)-> {
-                    float t=Mathf.absin(Time.time + Mathf.randomSeed(b.id, 0, 1000000), 1f, 1F)* 0.9f+0.1f;
-                   return Pal.heal.cpy().lerp(Color.black, t * (1f - b.healthf()));
+                colorFunc = (b) -> {
+                    float t = Mathf.absin(Time.time + Mathf.randomSeed(b.id, 0, 1000000), 1f, 1F) * 0.9f + 0.1f;
+                    return Pal.heal.cpy().lerp(Color.black, t * (1f - b.healthf()));
                 };
                 this.size = 2;
-                this.health=10000;
+                this.health = 10000;
                 this.laserRange = 6.0F;
                 this.requirements(Category.distribution, BuildVisibility.sandboxOnly, ItemStack.with(Items.copper, 3, Items.silicon, 10));
                 this.buildCostMultiplier = 4.0F;
