84ed1bf061d1e7076e2dc897de59e66406c53f19
Zelaux <ura20060605@gmail.com>
2021-02-10 10:43:32 +0300

    Added names for SectorPresets

--- core/src/braindustry/content/ModSectorPresent.java
+++ core/src/braindustry/content/ModSectorPresent.java
@@ -11,14 +11,16 @@ public class ModSectorPresent {
         Planet osorePlanet = Vars.content.planets().find(p -> p.name.toLowerCase().contains("osore"));
         meltingPoint = new SectorPreset("melting-point", osorePlanet, 20){
             {
+                localizedName="Melting point";
             alwaysUnlocked = true;
             difficulty = 6;
             captureWave = 20;
             }
         };
 
-        FFOf = new SectorPreset("451F", osorePlanet, 50){
+        FFOf = new SectorPreset("451f", osorePlanet, 50){
             {
+                localizedName="451F";
                alwaysUnlocked = true;
                difficulty = 8;
                captureWave = 35;
