99d4f2b9c835b818af5537772e6eabdf00ede82e
Zelaux <ura20060605@gmail.com>
2021-02-10 10:32:20 +0300

    Added all planets from scripts

--- core/src/braindustry/content/PlanetsFromScripts.java
+++ core/src/braindustry/content/PlanetsFromScripts.java
@@ -1,20 +1,48 @@
 package braindustry.content;
 
 import arc.graphics.Color;
+import arc.math.Mathf;
+import braindustry.maps.generators.MizuGenerator;
+import braindustry.maps.generators.OsorePlanetGenerator;
 import mindustry.content.Planets;
+import mindustry.graphics.g3d.HexMesh;
 import mindustry.graphics.g3d.SunMesh;
 import mindustry.type.Planet;
 
 public class PlanetsFromScripts {
-    public static Planet zetsubo;
+    public static Planet zetsubo, osore, mizu;
     public void load(){
         zetsubo=new Planet("zetsubo", Planets.sun,3,2.4f){
             {
                 hasAtmosphere = false;
-                meshLoader = () -> new SunMesh(zetsubo, 4, 5, 0.3, 1.7, 1.2, 1, 1.5f, Color.valueOf("9FFCFFFF"), Color.valueOf("50DDE2FF"), Color.valueOf("4749C9FF"));
+                meshLoader = () -> new SunMesh(this, 4, 5, 0.3f, 1.7f, 1.2f, 1, 1.5f, Color.valueOf("9FFCFFFF"), Color.valueOf("50DDE2FF"), Color.valueOf("4749C9FF"));
                 orbitRadius = 55.7f;
                 accessible = false;
                 bloom = true;
+            }
+        };
+        osore =new Planet("Osore",zetsubo,3,0.7f){
+            {
+                generator=new OsorePlanetGenerator();
+                startSector=25;
+               hasAtmosphere = false;
+               meshLoader = () -> new HexMesh(this, 8);
+               orbitRadius = 11.2f;
+               rotateTime = Float.POSITIVE_INFINITY; //ÍÈÊÎÃÄÀ
+               orbitTime = Mathf.pow((2.0f + 14.0f + 0.66f), 1.5f) * 80;
+               accessible = true;
+            }
+        };
+        mizu=new Planet("Mizu", zetsubo, 3, 1f){
+            {
+                generator =new MizuGenerator();
+                startSector = 25;
+                hasAtmosphere = false;
+                meshLoader = () -> new HexMesh(this, 8);
+                orbitRadius = 21.7f;
+                rotateTime = 24;
+                orbitTime = Mathf.pow((2.0f + 14.0f + 0.66f), 1.5f) * 80;
+                accessible = true;
             }
         };
     }
---
+++ core/src/braindustry/maps/generators/MizuGenerator.java
@@ -0,0 +1,40 @@
+package braindustry.maps.generators;
+
+import arc.struct.ObjectMap;
+import braindustry.content.ModBlocks;
+import mindustry.content.Blocks;
+import mindustry.world.Block;
+
+public class MizuGenerator extends ModPlanetGenerator {
+    public MizuGenerator() {
+        arr = new Block[][]{
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.craters, Blocks.grass, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.grass, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.grass, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.grass, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water},
+                {Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.water, Blocks.grass, Blocks.water, Blocks.water, Blocks.water}
+        };
+        scl=2f;
+        waterOffset=0.01f;
+        water=0.05f;
+        dec= ObjectMap.of(
+                Blocks.water, Blocks.water,
+                Blocks.water, Blocks.water,
+                Blocks.water, Blocks.water,
+                Blocks.water, Blocks.water
+        );
+        tars = ObjectMap.of(
+                Blocks.grass, Blocks.grass,
+                Blocks.grass, Blocks.grass
+        );
+    }
+
+}
