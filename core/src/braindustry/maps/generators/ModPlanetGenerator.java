ed8d5dee2de880796fab3cd037c80be9fbb9f3f9
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-03-22 20:04:42 +0300

    прост

--- core/src/braindustry/maps/generators/ModPlanetGenerator.java
+++ core/src/braindustry/maps/generators/ModPlanetGenerator.java
@@ -20,7 +20,7 @@ public class ModPlanetGenerator extends PlanetGenerator {
     public  float scl = 0.f;
     public  float waterOffset = 0.f;
     public   BaseGenerator basegen = new BaseGenerator();
-   public float water = 0.f;
+    public float water = 0.f;
     ObjectMap<Block,Block> dec;
 
     ObjectMap<Block,Block> tars;
@@ -82,5 +82,67 @@ public class ModPlanetGenerator extends PlanetGenerator {
         if (rid.getValue(position.x, position.y, position.z, 22) > 0.32) {
             tile.block = Blocks.air;
         }
+    }
+    
+    public abstract class PlanetGenerator extends BasicGenerator implements HexMesher{
+    protected IntSeq ints = new IntSeq();
+    protected Sector sector;
+    protected Simplex noise = new Simplex();
+
+    /** Should generate sector bases for a planet. */
+    public void generateSector(Sector sector){
+        Ptile tile = sector.tile;
+
+        boolean any = false;
+        float noise = Noise.snoise3(tile.v.x, tile.v.y, tile.v.z, 0.001f, 0.5f);
+
+        if(noise > 0.027){
+            any = true;
+        }
+
+        if(noise < 0.15){
+            for(Ptile other : tile.tiles){
+                //no sectors near start sector!
+                if(sector.planet.getSector(other).id == sector.planet.startSector){
+                    return;
+                }
+                
+                if(sector.planet.getSector(other).generateEnemyBase){
+                    any = false;
+                    break;
+                }
+            }
+        }
+
+        if(any){
+            sector.generateEnemyBase = true;
+        }
+    }
+
+    protected void genTile(Vec3 position, TileGen tile){
+
+    }
+
+    @Override
+    protected float noise(float x, float y, double octaves, double falloff, double scl, double mag){
+        Vec3 v = sector.rect.project(x, y);
+        return (float)noise.octaveNoise3D(octaves, falloff, 1f / scl, v.x, v.y, v.z) * (float)mag;
+    }
+
+    public void generate(Tiles tiles, Sector sec){
+        this.tiles = tiles;
+        this.sector = sec;
+        this.rand.setSeed(sec.id);
+
+        TileGen gen = new TileGen();
+        tiles.each((x, y) -> {
+            gen.reset();
+            Vec3 position = sector.rect.project(x / (float)tiles.width, y / (float)tiles.height);
+
+            genTile(position, gen);
+            tiles.set(x, y, new Tile(x, y, gen.floor, gen.overlay, gen.block));
+        });
+
+        generate(tiles);
     }
 }
